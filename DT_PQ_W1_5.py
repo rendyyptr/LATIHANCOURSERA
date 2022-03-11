93d38326ad044855052360f2cace4adb02ebdb15
Rendy <rendyputra4578@gmail.com>
2022-03-11 09:00:04 +0700

    INI FILE DT PQ W1 NOMOR 5

---
+++ DT_PQ_W1_5.py
@@ -0,0 +1,49 @@
+def linear_search(list, key):
+    #Returns the number of steps to determine if key is in the list 
+
+    #Initialize the counter of steps
+    steps=0
+    for i, item in enumerate(list):
+        steps += 1
+        if item == key:
+            break
+    return steps
+
+def binary_search(list, key):
+    #Returns the number of steps to determine if key is in the list 
+
+    #List must be sorted:
+    list.sort()
+
+    #The Sort was 1 step, so initialize the counter of steps to 1
+    steps=1
+
+    left = 0
+    right = len(list) - 1
+    while left <= right:
+        steps += 1
+        middle = (left + right) // 2
+        
+        if list[middle] == key:
+            break
+        if list[middle] > key:
+            right = middle - 1
+        if list[middle] < key:
+            left = middle + 1
+    return steps
+
+def best_search(list, key):
+    steps_linear = linear_search(list,key)
+    steps_binary = binary_search(list,key)
+    results = "Linear: " + str(steps_linear) + " steps, "
+    results += "Binary: " + str(steps_binary) + " steps. "
+    if (steps_linear < steps_binary):
+        results += "Best Search is Linear."
+    elif (steps_binary < steps_linear):
+        results += "Best Search is Binary."
+    else:
+        results += "Result is a Tie."
+
+    return results
+
+print(best_search([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 1))
